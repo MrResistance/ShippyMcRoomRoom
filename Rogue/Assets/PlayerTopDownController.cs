e21c08746ffc4da4d255846e63b2645556f04a20
floatPond <32678960+floatPond@users.noreply.github.com>
2020-07-28 14:39:22 +0100

    SC - Added new top-down player controller
    Pretty basic, just to get movement in. Could probably later be merged into the already established 'Player' one

---
+++ Rogue/Assets/PlayerTopDownController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTopDownController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+    public Camera camera;
+
+    public Vector2 mouseposition;
+    public Vector2 lookdir;
+    public float angle,h,v;
+
+    public float movementspeed;
+    public float movementspeedbonus;
+    void Start()
+    {
+        movementspeedbonus = 100;
+        rb = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        mouseposition = camera.ScreenToWorldPoint(Input.mousePosition);
+        lookdir = mouseposition - rb.position;
+        angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+        rb.AddForce(transform.up * v * speed());
+    }
+    private float speed()
+    {
+        return movementspeed * (movementspeedbonus / 100);
+    }
+}
