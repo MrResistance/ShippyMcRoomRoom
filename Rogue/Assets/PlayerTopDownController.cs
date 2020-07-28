27ee4fc301db4bb49d7caf12b97e9fe6c7730f31
floatPond <32678960+floatPond@users.noreply.github.com>
2020-07-28 14:45:12 +0100

    SC - Improved code
    -Put stuff into their own methods to be all tidy-like.
    -Added boolean to change how the player can move itself

--- Rogue/Assets/PlayerTopDownController.cs
+++ Rogue/Assets/PlayerTopDownController.cs
@@ -6,6 +6,8 @@ public class PlayerTopDownController : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public bool isDirectionalMovement; //If true, player moves based on where they look; else, upie downlie lefty rightie
+
     public Rigidbody2D rb;
     public Camera camera;
 
@@ -17,7 +19,7 @@ public class PlayerTopDownController : MonoBehaviour
     public float movementspeedbonus;
     void Start()
     {
-        movementspeedbonus = 100;
+        movementspeedbonus = 100; //Default start
         rb = GetComponent<Rigidbody2D>();
         camera = Camera.main;
     }
@@ -25,17 +27,35 @@ public class PlayerTopDownController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
-
-        mouseposition = camera.ScreenToWorldPoint(Input.mousePosition);
-        lookdir = mouseposition - rb.position;
-        angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
-        rb.AddForce(transform.up * v * speed());
+        Look();
+        Move();
     }
     private float speed()
     {
         return movementspeed * (movementspeedbonus / 100);
+    }
+
+    void Look()
+    {
+        mouseposition = camera.ScreenToWorldPoint(Input.mousePosition);
+        lookdir = mouseposition - rb.position;
+        angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+    void Move()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        if (isDirectionalMovement)
+        {
+            rb.AddForce(transform.up * v * speed());
+            rb.AddForce(transform.right * h * speed());
+        }
+        else
+        {
+            rb.AddForce(Vector2.up * v * speed());
+            rb.AddForce(Vector2.right * h * speed());
+        }
+
     }
 }
