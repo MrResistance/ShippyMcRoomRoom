0f406349038f89f897122f8216ef4376784bc956
MrResistance <Paul1208Gush@Gmail.com>
2020-07-28 22:35:15 +0100

    Weapon now fires with Right Trigger on Controller
    Also added physics layers

---
+++ Rogue/Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float destroyTimer = 15f;
+    private void Start()
+    {
+        Destroy(this.gameObject, destroyTimer);
+    }
+}
