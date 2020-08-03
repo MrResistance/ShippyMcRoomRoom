1c7a2264cdaf0d79737b5f80107fdc81e0eda415
MrResistance <Paul1208Gush@Gmail.com>
2020-08-04 00:26:07 +0100

    Added Health Collision code

--- Rogue/Assets/Scripts/EntityHealth.cs
+++ Rogue/Assets/Scripts/EntityHealth.cs
@@ -6,20 +6,50 @@ public class EntityHealth : MonoBehaviour
 {
     //What is this
     //This script is for entity health. This also includes the player.
-
+    private bool isEnemy, isPlayer;
     //Variables
     public float health; //VERY BASIC FOR NOW - Later versions will have different stuff
     void Start()
     {
-        
+        if (this.gameObject.tag.Contains("Player"))
+        {
+            isPlayer = true;
+        }
+        else if (this.gameObject.tag.Contains("Enemy"))
+        {
+            isEnemy = true;
+        }
     }
 
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
 
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            //End game
+        }
+    }
 
-    //Have its own colission detection
-        //If is player
-            //If has been hit by non-player projectile
-            //Did it damage me lol
-                //Take from health
-                    //Did it damage me enough for me to kill me
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isEnemy && collision.otherCollider.tag.Contains("Player Projectile"))
+        {
+            //TakeDamage(collision.otherCollider.Projectile.damage);
+        }
+        else if (isPlayer && collision.otherCollider.tag.Contains("Enemy Projectile"))
+        {
+            //TakeDamage(collision.otherCollider.Projectile.damage);
+        }
+    }
+
+    //Have its own collision detection
+    //If is player
+    //If has been hit by non-player projectile
+    //Did it damage me lol
+    //Take from health
+    //Did it damage me enough for me to kill me
 }
