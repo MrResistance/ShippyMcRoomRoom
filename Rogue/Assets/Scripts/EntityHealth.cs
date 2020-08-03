223371d1d8e4575f8bfba9a956b89bc400b87ec6
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-03 22:21:28 +0100

    SC - Added Entity Health script

---
+++ Rogue/Assets/Scripts/EntityHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealth : MonoBehaviour
+{
+    //What is this
+    //This script is for entity health. This also includes the player.
+
+    //Variables
+    public float health; //VERY BASIC FOR NOW - Later versions will have different stuff
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
