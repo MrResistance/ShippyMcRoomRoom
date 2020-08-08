000c02885f794bb9f7511833112dd14cc8fb507f
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-08 15:37:58 +0100

    SC - Added UIManager for in-game, changes wave number

---
+++ Rogue/Assets/GameUIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI t_wavenumber;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeWaveNumber(int number)
+    {
+        if (number != 0)
+        {
+            t_wavenumber.text = "Wave " + number.ToString();
+        }
+        else
+            t_wavenumber.text = "Boss Level";
+
+    }
+}
