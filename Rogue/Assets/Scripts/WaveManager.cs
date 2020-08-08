1542320eea32cd12ed0a1d7b5086ffeda4926a96
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-08 16:41:52 +0100

    SC - WaveManager now spawns enemies and changes wave

--- Rogue/Assets/Scripts/WaveManager.cs
+++ Rogue/Assets/Scripts/WaveManager.cs
@@ -7,10 +7,15 @@ public class WaveManager : MonoBehaviour
     public int WaveNumber;
     public int remainingEnemies;
     public bool isWaveChanging;
+
+    public GameObject EnemiesGO;
+    public GameObject EnemyPrefab;
+
+    public List<GameObject> listEnemies;
     void Start()
     {
         NextWave();
-
+        StartCoroutine(WaveUpdate());
     }
 
     // Update is called once per frame
@@ -23,6 +28,7 @@ public class WaveManager : MonoBehaviour
     {
         while (true)
         {
+            CountEnemies();
             if (remainingEnemies == 0 && isWaveChanging == false)
             {
                 isWaveChanging = true;
@@ -34,10 +40,28 @@ public class WaveManager : MonoBehaviour
 
     public void NextWave()
     {
+        Debug.Log("Next wave!");
         WaveNumber++;
         this.gameObject.GetComponent<GameUIManager>().ChangeWaveNumber(WaveNumber);
-        remainingEnemies += 1;
+        SpawnEnemies();
         isWaveChanging = false; 
-
+    }
+    void SpawnEnemies()
+    {
+        Transform EnemySpawn = transform;
+        
+        for (int e = 0; e < WaveNumber;e++)
+        {
+            EnemySpawn.position = new Vector3(-40f+(e*10), 25f,0f);
+            GameObject Enemy = Instantiate(EnemyPrefab, EnemySpawn.position, EnemySpawn.rotation);
+            Enemy.transform.parent = EnemiesGO.transform;
+            listEnemies.Add(Enemy);
+        }
+    }
+    void CountEnemies()
+    {
+        
+        remainingEnemies = EnemiesGO.transform.childCount;
+        Debug.Log("Enemy count is: " + remainingEnemies.ToString());
     }
 }
