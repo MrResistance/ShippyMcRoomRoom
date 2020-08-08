a4ae87589d4cfd98f0b8d2c17929bc6189fa3229
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-08 15:39:55 +0100

    SC - Some logic to change wave number

--- Rogue/Assets/Scripts/WaveManager.cs
+++ Rogue/Assets/Scripts/WaveManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int WaveNumber;
+    public int remainingEnemies;
+    public bool isWaveChanging;
     void Start()
     {
-        
+        NextWave();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator WaveUpdate()
+    {
+        while (true)
+        {
+            if (remainingEnemies == 0 && isWaveChanging == false)
+            {
+                isWaveChanging = true;
+                NextWave();
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void NextWave()
+    {
+        WaveNumber++;
+        this.gameObject.GetComponent<GameUIManager>().ChangeWaveNumber(WaveNumber);
+        remainingEnemies += 1;
+        isWaveChanging = false; 
+
     }
 }
