3e7af2b80aa4504aacac346bf50ced7541cbc9ae
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-14 19:55:02 +0100

    SC - Added buffer to damage

--- Rogue/Assets/Scripts/Projectile.cs
+++ Rogue/Assets/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float damage, destroyTimer = 10f;
+    public float permdamange;
     public SpriteRenderer sr;
     private void Awake()
     {
@@ -25,19 +26,25 @@ public class Projectile : MonoBehaviour
         if (collision.gameObject.tag.Contains("Projectile"))
         {
             //this.gameObject.GetComponent<EntityHealth>().TakeDamage(damage);
-            collision.gameObject.GetComponent<EntityHealth>().TakeDamage(damage);
-            Debug.Log("Giving " + damage.ToString() + " to " + collision.gameObject.name);
+            collision.gameObject.GetComponent<EntityHealth>().TakeDamage(CalculateDamange());
+            //Debug.Log("Giving " + CalculateDamange().ToString() + " to " + collision.gameObject.name);
             //Destroy(collision.gameObject);
             //Destroy(this.gameObject);
         }
         //Check if enemy
         if (collision.gameObject.tag == ("Player") || collision.gameObject.tag == ("Enemy"))
         {
-            collision.gameObject.GetComponent<EntityHealth>().TakeDamage(damage);
+            collision.gameObject.GetComponent<EntityHealth>().TakeDamage(CalculateDamange());
             sr.enabled = false;
             Destroy(this.gameObject, 3);
         }
         //sr.enabled = false;
         //Destroy(this.gameObject, 3);
+    }
+    public float CalculateDamange()
+    {
+        float number;
+        number = damage + permdamange;
+        return number;
     }
 }
