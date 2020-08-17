15bc8d282b8de683dff8c987ad3071ff39120c13
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-17 22:06:43 +0100

    SC - Started new weapons script
    Idk if it'll work. Core stuff has been included for now

---
+++ Rogue/Assets/Scripts/Weapons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class Weapon
+{
+    public string name; //Weapon name
+    public int damage; //Damage of projectile on-impact
+    public float rateoffire;
+    public float speed;
+    public float chargetime; //How long it takes to charge the weapon for it to fire
+    public float spread; //Accuracy; 
+    public float range; //How far the projectile travels before it either evaporates or explodes
+    public bool isUnlimitedAmmo;
+    public int ammo;
+
+    //Explosion stuff
+    public bool isExplosiveProjectile; //Required to be true for any explosive stuff
+    public string explosionType; //"Throughout" - same damage to the end -- "Degrading" - damage more towards center
+    public bool isExplosionOnImpact;
+    public float explosiveradius;
+    public int explosivedamage;
+
+    //Scattered projectiles - fireworks
+    public bool isScatteredProjectilesOnImpact;
+    public string scatteredprojectiletype; //"even" - Always same dispersion -- "random" - they can fly out anywhere
+    public int scatteredprojectilecount;
+    public float scatteredprojectilespeed;
+    public float scatteredprojectilerange;
+    public int scattedprojectiledamage;
+    
+}
