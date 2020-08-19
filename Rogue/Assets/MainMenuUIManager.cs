866e879ebaf45fd480e2e4207276184a59b590a0
MrResistance <Paul1208Gush@Gmail.com>
2020-08-19 16:04:15 +0100

    Main Menu added (Will make adjustments later)

---
+++ Rogue/Assets/MainMenuUIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuUIManager : MonoBehaviour
+{
+    public void NextScene()
+    {
+        int currentScene;
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
