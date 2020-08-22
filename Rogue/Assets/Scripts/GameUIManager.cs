344909ef57dbff93fa8e90060c55efa9d6419ed8
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-22 19:56:37 +0100

    SC - it now floats
    also removed old code

--- Rogue/Assets/Scripts/GameUIManager.cs
+++ Rogue/Assets/Scripts/GameUIManager.cs
@@ -14,7 +14,7 @@ public class GameUIManager : MonoBehaviour
     public Sprite buttonDefaultSprite;
     public int ItemNumberChosen; //1, 2, or 3
     public string[] upgrades;
-    public int[] upgradeamount;
+    public float[] upgradeamount;
     public bool ShowingRewards = false;
     public Button item1;
     public TextMeshProUGUI item1txt, item2txt, item3txt;
@@ -27,7 +27,7 @@ public class GameUIManager : MonoBehaviour
     void Start()
     {
         upgrades = new string[] { "movement", "attackspeed", "damage" };
-        upgradeamount = new int[] { 5, 5, 5 };
+        upgradeamount = new float[] { 5, 5, 5 };
         //InvokeRepeating("Pond", 2f, 3f);
     }
     void Pond()
@@ -53,14 +53,15 @@ public class GameUIManager : MonoBehaviour
         if (number != 0)
         {
             t_wavenumber.text = "Wave " + number.ToString();
-            upgradeamount = new int[] { 5 * number, 5 * number, 5 * number };
+            upgradeamount = new float[] { 5 * (1 + (number / 10)), 5 * (1 + (number / 10)), 5 * (1 + (number / 10)) };
+            Debug.Log(upgradeamount);
             item1txt.text = ("Movement Speed +" + upgradeamount[0]);
             item2txt.text = ("Attack Speed +" + upgradeamount[1]);
             item3txt.text = ("Attack Damage +" + upgradeamount[2]);
         }
         else
             t_wavenumber.text = "Boss Level";
-            upgradeamount = new int[] { 5 * number, 5 * number, 5 * number };
+            upgradeamount = new float[] { 5 * number, 5 * number, 5 * number };
 
     }
     public void ConfirmRewardNextWave()
@@ -78,21 +79,6 @@ public class GameUIManager : MonoBehaviour
             this.gameObject.GetComponent<WaveManager>().NextWave();
         }
     }
-    /*public void SwapPanel() //Not used now 'cause it just switched between two
-    {
-        if (UI.transform.GetChild(0).gameObject.activeInHierarchy) //If Game HUD is enabled, swap for reward
-        {
-            UI.transform.GetChild(0).gameObject.SetActive(false);
-            UI.transform.GetChild(1).gameObject.SetActive(true);
-            Debug.Log("Swapping to Rewards Panel");
-        }
-        else
-        {
-            UI.transform.GetChild(0).gameObject.SetActive(true);
-            UI.transform.GetChild(1).gameObject.SetActive(false);
-            Debug.Log("Swapping to Game Panel");
-        }
-    }*/
     public void SwapPanel(int panel) //Superior because you can specify
     {
         switch(panel)
@@ -122,9 +108,9 @@ public class GameUIManager : MonoBehaviour
         ci = upgrades[ItemNumberChosen-1]; //minus 1 because buttons start at 1 and array starts at 0
         return ci;
     }
-    int GetChosenItemAmount()
+    float GetChosenItemAmount()
     {
-        int ca;
+        float ca;
         ca = upgradeamount[ItemNumberChosen-1]; //minus 1 because buttons start at 1 and array starts at 0
         return ca;
             
