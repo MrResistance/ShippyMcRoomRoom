74ae207138d5ee1dcba0987bc4769f28d9bc234d
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-22 21:19:43 +0100

    SC - Added stuff to EnemyData

--- Rogue/Assets/Scripts/ScriptableObjects/EnemyData.cs
+++ Rogue/Assets/Scripts/ScriptableObjects/EnemyData.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptables/EnemyData")]
 public class EnemyData : ScriptableObject
 {
-  //Not yet used
+    public string name;
+    public string description;
+    //sprite
+    public Sprite sprite;
+    //movement
+    public float speedMovement;
+    public float speedRotation;
+    //other
+    public float healthMaximum;
+
 }
