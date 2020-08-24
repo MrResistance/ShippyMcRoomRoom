ce7a473ffe82a5d7beaa3248eb0088336626146e
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-24 16:51:25 +0100

    SC - Added things to WeaponData
    Sprite, colour of sprite, scale, projectile lifetime and projectile tracking

--- Rogue/Assets/Scripts/ScriptableObjects/WeaponData.cs
+++ Rogue/Assets/Scripts/ScriptableObjects/WeaponData.cs
@@ -7,6 +7,10 @@ public class WeaponData : ScriptableObject
 {
     public string name; //Weapon name
     public string description;
+    public Sprite sprite; //For projectile
+    public Color colourSprite;
+    public float scale; //for size
+
     public int damage; //Damage of projectile on-impact
     public float rateoffire;
     public float speed;
@@ -16,7 +20,7 @@ public class WeaponData : ScriptableObject
     public bool isUnlimitedAmmo;
     public int ammo;
     public int healthProjectile;
-
+    public float lifetimeProjectile; //In seconds, how long projectile will last before dying - if 0, it will only die on impact (or leaving combat area)
     //Explosion stuff
     public bool isExplosiveProjectile; //Required to be true for any explosive stuff
     public string explosionType; //"Throughout" - same damage to the end -- "Degrading" - damage more towards center
@@ -35,6 +39,10 @@ public class WeaponData : ScriptableObject
     //Piercing
     public bool canPierce;
     public int amountCanPierce;
+
+    //Tracking - for missiles and anything else we want the projectile to track
+    public bool isTrackingProjectile;
+    public float trackingRotationSpeed;
 
 }
 
