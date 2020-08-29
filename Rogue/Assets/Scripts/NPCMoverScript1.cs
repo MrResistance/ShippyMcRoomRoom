221614c0fb18754227ac496ea1f43075ac8ad686
floatPond <32678960+floatPond@users.noreply.github.com>
2020-08-29 16:45:37 +0100

    SC - Basic NPC functionality for attacking both sides 🎉🎉🎉🎉🎉🎉🎉🥳👯‍♂️👯‍♀️🎉

--- Rogue/Assets/Scripts/NPCMoverScript1.cs
+++ Rogue/Assets/Scripts/NPCMoverScript1.cs
@@ -7,7 +7,9 @@ using Pathfinding;
 
 public class NPCMoverScript1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string npcSide; //"enemy","allied"
+    public WaveManager wm; //For getting the most updated lists of enemies to npc
+
     public EnemyData enemyData;
     public WeaponData wd;
 
@@ -19,110 +21,49 @@ public class NPCMoverScript1 : MonoBehaviour
 
     float timeElapsed;
     bool isMoving;
-    public GameObject player;
-    Vector2 distanceToPlayer;
-    public float distanceToPlayerfloat;
+
+    //New targetting
+    public GameObject target;
+    Vector2 distanceToTarget;
+    public float distanceToTargetFloat;
 
     Rigidbody2D rb;
     public Transform firePoint; //For firing from
-    public float maximumShootingDistance, projectileSpeed = 1f;
+
+    //To be outdated via getting the max distance one can shoot from the weapon
+    //Max shooting distance should be projectile wd.speed x wd.projectileLifetime
+    public float maximumShootingDistance;
     public GameObject projectilePrefab, 
         tHealth; //text health
     public AudioSource audioSource;
-
+    //The GO parent where all projectiles are stored
     public GameObject ProjectilesGO;
 
     float offsetRateOfFire; //To stop enemies shooting at the same speed
+
+   public int wavenumberSpawnedIn;
+    /// <summary>
+    /// /////////////////////////////////////////////// METHODS /////////////////////////////////////////////
+    /// </summary>
+    /// 
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         audioSource = gameObject.GetComponent<AudioSource>();
-
     }
 
     // Update is called once per frame
+
     private void Start()
     {
         SetupNPC();
-    }
-
-    void Update()
-    {
-        //Move(); //Does stuff in A*
-        Rotate();
-        tHealth.GetComponent<TextMeshProUGUI>().text = GetComponent<EntityHealth>().health.ToString("F1");
-    }
-
-    IEnumerator TargetPlayer()
-    {
-        while (true)
-        {
-            if (player != null)
-            {
-                GetComponent<AIDestinationSetter>().target = player.transform;
-                if (CanShoot())
-                {
-                    ShootEnemyOfNPC();
-                }
-            }
-            else
-            {
-                player = GameObject.Find("Player");
-            }
-            yield return new WaitForSeconds(wd.rateoffire+ offsetRateOfFire);
-        }
-    }
-    IEnumerator TargetEnemyOfNPC()
-    {
-        while (true)
-        {
-            if (player != null)
-            {
-                GetComponent<AIDestinationSetter>().target = player.transform;
-                if (CanShoot())
-                {
-                    ShootEnemyOfNPC();
-                }
-            }
-            else
-            {
-                player = GameObject.Find("Player");
-            }
-            yield return new WaitForSeconds(wd.rateoffire + offsetRateOfFire);
-        }
-    }
-    void ShootEnemyOfNPC()
-    {
-        //Debug.Log("Firing at player");
-        audioSource.PlayOneShot(audioSource.clip);
-        for (int p = 0; p < wd.projectileCount; p++)
-        { 
-            GameObject projectile = Instantiate(projectilePrefab, firePoint.transform.position, WeaponFiringAngle());
-            projectile.GetComponent<Projectile>().firepoint = firePoint.transform;
-            //Temp layer specifying - this needs to be changed later should we want to use this script to make friendly NPCs
-            projectile.layer = 11; //The Enemy Projectile layer
-            projectile.GetComponent<Projectile>().wd = wd;
-            projectile.transform.parent = ProjectilesGO.transform;
-            if (wd.spreadType == "even")
-            {
-                projectile.GetComponent<Projectile>().spreadNumber = p;
-            }
-        }
-    }
-    void GetDistanceToPlayer()
-    {
-        if (player != null)
-        {
-            distanceToPlayer = player.transform.position - transform.position;
-            distanceToPlayerfloat = Vector2.Distance(player.transform.position, transform.position);
-            //Debug.Log(distanceToPlayerfloat);
-        }
     }
     void SetupNPC()
     {
         firePoint = transform.GetChild(0);
         tHealth = transform.GetChild(1).GetChild(0).gameObject;
-        StartCoroutine(TargetEnemyOfNPC());
+        distanceToTargetFloat = Mathf.Infinity;
         this.gameObject.GetComponent<EntityHealth>().health = enemyData.healthMaximum;
         this.gameObject.GetComponent<EntityHealth>().healthMaximum = enemyData.healthMaximum;
         this.gameObject.GetComponent<SpriteRenderer>().sprite = enemyData.sprite;
@@ -132,21 +73,57 @@ public class NPCMoverScript1 : MonoBehaviour
         float low = (wd.rateoffire / 100) * 5f * -1f;
         float high = (wd.rateoffire / 100) * 5f;
         offsetRateOfFire = Random.RandomRange(low, high);
-    }
-    void Move()
-    {
+        //For getting a target
+        StartCoroutine(TargetEnemyOfNPC());
+        StartCoroutine(ShootEnemyOfNPC());
         
     }
+
+    void Update()
+    {
+        //Move(); //Does stuff in A*
+        Rotate();
+        tHealth.GetComponent<TextMeshProUGUI>().text = GetComponent<EntityHealth>().health.ToString("F1");
+    }
+    
+    void ShootTarget()
+    {
+        //Debug.Log("Firing at player");
+        audioSource.PlayOneShot(audioSource.clip);
+        for (int p = 0; p < wd.projectileCount; p++)
+        { 
+            GameObject projectile = Instantiate(projectilePrefab, firePoint.transform.position, WeaponFiringAngle());
+            projectile.GetComponent<Projectile>().firepoint = firePoint.transform;
+            //Temp layer specifying - this needs to be changed later should we want to use this script to make friendly NPCs
+            //projectile.layer = 11; //The Enemy Projectile layer
+            if (npcSide == "enemy")
+            {
+                projectile.tag = "Enemy Projectile";
+                projectile.layer = 11;
+            }
+            if (npcSide == "allied")
+            {
+                projectile.tag = "Player Projectile";
+                projectile.layer = 10;
+            }
+            projectile.GetComponent<Projectile>().wd = wd;
+            projectile.transform.parent = ProjectilesGO.transform;
+            if (wd.spreadType == "even")
+            {
+                projectile.GetComponent<Projectile>().spreadNumber = p;
+            }
+        }
+    }
+    
     void Rotate()
     {
-        GetDistanceToPlayer();
-        if (player != null)
+        if (target != null)
         {
-            float angle = Mathf.Atan2(distanceToPlayer.y, distanceToPlayer.x) * Mathf.Rad2Deg - 90f;
+            GetDistanceToTarget();
+            float angle = Mathf.Atan2(distanceToTarget.y, distanceToTarget.x) * Mathf.Rad2Deg - 90f;
             Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
             Vector3 angle2 = transform.rotation.eulerAngles;
-
-            //Debug.Log("Angle =" + angle.ToString() + ",Angle2 = "+ angle2.ToString());
+            
             transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * enemyData.speedRotation);
         }
         else
@@ -157,7 +134,7 @@ public class NPCMoverScript1 : MonoBehaviour
     }
     bool isCloseEnoughToShoot() //So entity knows it can hit the target
     {
-        if (distanceToPlayerfloat < wd.range)
+        if (distanceToTargetFloat < wd.range)
         { 
             return true;
         }
@@ -201,4 +178,97 @@ public class NPCMoverScript1 : MonoBehaviour
         }
         return false;
     }
+    bool npcHasEnemies() //Used to determine whether there is something for the NPC to target
+    {
+        if (npcSide == "enemy")
+        {
+            if (wm.listAllies.Count != 0 && wm.AlliesGO.transform.childCount != 0)
+            {
+                return true;
+            }
+        }
+        else if (npcSide == "allied")
+        {
+            if (wm.listEnemies.Count != 0 && wm.EnemiesGO.transform.childCount != 0)
+            {
+                return true;
+            }
+        }
+        //No target available
+        distanceToTargetFloat = Mathf.Infinity; //So getting shorter distance will be easier
+        return false;
+    }
+
+    //For getting a target for the NPC
+    IEnumerator TargetEnemyOfNPC()
+    {
+        while (true)
+        {
+            if (npcHasEnemies() && wm.isWaveChanging == false)
+            {
+                if (npcSide == "enemy")
+                {
+                    //Get closest
+                    foreach (GameObject go in wm.listAllies)
+                    {
+                        if (go != null)
+                        { 
+                            Vector2 distanceToGO = go.transform.position - transform.position;
+                            float distanceToGOFloat = Vector2.Distance(go.transform.position, transform.position);
+                            if (distanceToGOFloat < distanceToTargetFloat)
+                            {
+                                target = go;
+                                GetComponent<AIDestinationSetter>().target = target.transform; //Moves NPC to target
+                            }
+                        }
+                    }
+                }
+                else if (npcSide == "allied")
+                {
+                    //Get closest
+                    //Make that the target
+                    foreach (Transform go in wm.EnemiesGO.transform)
+                    {
+                        if (go.gameObject != null)
+                        {
+                            Vector2 distanceToGO = go.transform.position - transform.position;
+                            float distanceToGOFloat = Vector2.Distance(go.transform.position, transform.position);
+                            if (distanceToGOFloat < distanceToTargetFloat)
+                            {
+                                target = go.gameObject;
+                                GetComponent<AIDestinationSetter>().target = target.transform; //Moves NPC to target
+                            }
+                        }
+                    }
+                }
+                if (target != null)
+                GetComponent<AIDestinationSetter>().target = target.transform; //Moves NPC to target
+                yield return new WaitForSeconds(wd.rateoffire * 3); //Ensures that the target is shot at least 3 times before switching
+            }
+            else //No enemies for NPC available
+            {
+                //Guess no target.
+                yield return new WaitForSeconds(1f); //Waits one second before trying again
+            }
+            
+        }
+    }
+    //For shooting the target
+    IEnumerator ShootEnemyOfNPC()
+    {
+        while (true)
+        {
+            if (target != null && CanShoot())
+            {
+                ShootTarget();
+            }
+            yield return new WaitForSeconds(wd.rateoffire + offsetRateOfFire);
+        }
+    }
+    void GetDistanceToTarget()
+    {
+        distanceToTarget = target.transform.position - transform.position;
+        distanceToTargetFloat = Vector2.Distance(target.transform.position, transform.position);
+    }
+
 }
