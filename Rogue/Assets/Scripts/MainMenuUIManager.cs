9f88e7a5392e54eee961793809d76cc9f990717d
MrResistance <Paul1208Gush@Gmail.com>
2020-09-17 00:39:57 +0100

    More updates to main menu

--- Rogue/Assets/Scripts/MainMenuUIManager.cs
+++ Rogue/Assets/Scripts/MainMenuUIManager.cs
@@ -7,11 +7,12 @@ using UnityEngine.UI;
 
 public class MainMenuUIManager : MonoBehaviour
 {
-    public GameObject defaultButton;
+    public GameObject defaultButton, scrollingBackgroundObj;
     public Animator playerShipAnim, buttonsAnim;
     private void Awake()
     {
         defaultButton = GameObject.FindGameObjectWithTag("Default Button");
+        scrollingBackgroundObj = GameObject.Find("ScrollingBackground");
     }
     public void HighlightButton()
     {
@@ -28,6 +29,11 @@ public class MainMenuUIManager : MonoBehaviour
     {
         playerShipAnim.SetTrigger("FlyOff");
         buttonsAnim.SetTrigger("FlyOff");
+        yield return new WaitForSeconds(1.5f);
+        foreach (Transform child in scrollingBackgroundObj.transform)
+        {
+            child.GetComponent<ScrollingBackground>().ChangeBgSpeed(-50f);
+        }
         yield return new WaitForSeconds(3.5f);
         int currentScene;
         currentScene = SceneManager.GetActiveScene().buildIndex;
