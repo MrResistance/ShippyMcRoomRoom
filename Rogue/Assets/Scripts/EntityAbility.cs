d6c5a2c83e9ba78dcf6860494bac7a028eb99c99
floatPond <32678960+floatPond@users.noreply.github.com>
2020-09-26 20:20:03 +0100

    SC - Added support classes for abilities

---
+++ Rogue/Assets/Scripts/EntityAbility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityAbility : MonoBehaviour
+{
+    //Variables
+    public string side;
+
+
+    protected virtual void Start()
+    {
+        //Get Side
+        Debug.Log("Yes!");
+        GetSide();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void GetSide()
+    {
+        if (transform.parent.gameObject.GetComponent<PlayerTopDownController>() != null)
+        {
+            side = "allied";
+        }
+        if (transform.parent.gameObject.GetComponent<NPCMoverScript1>() != null)
+        {
+            side = transform.parent.gameObject.GetComponent<NPCMoverScript1>().npcSide;
+        }
+    }
+}
