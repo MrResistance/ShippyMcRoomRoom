28a963d3718514362927c2e2fd99b1f1d9b37796
floatPond <32678960+floatPond@users.noreply.github.com>
2020-09-27 11:34:33 +0100

    SC - Started implementing logic that'll be invoked on all Active Abilities

---
+++ Rogue/Assets/Scripts/Abilities/ActiveAbility.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveAbility : EntityAbility
+{
+
+    public bool isSelected = false;
+
+    public bool isAbilityReady = true;
+    public float abilityCooldown = 0;
+    public float abilityTimeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateCooldown()
+    {
+        isAbilityReady = false;
+        abilityTimeRemaining = abilityCooldown;
+        Invoke("DeactivateCooldown", abilityCooldown);
+        
+    }
+
+    void DeactivateCooldown()
+    {
+        isAbilityReady = true;
+        abilityTimeRemaining = 0;
+    }
+}
