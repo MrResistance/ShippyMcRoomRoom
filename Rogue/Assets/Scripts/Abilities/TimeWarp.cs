84a01c18df1d05bf6d87a03e7caa9fc514621ea5
floatPond <32678960+floatPond@users.noreply.github.com>
2020-09-27 11:33:56 +0100

    SC - Started updating Time Warp as an Active Ability

---
+++ Rogue/Assets/Scripts/Abilities/TimeWarp.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeWarp : ActiveAbility
+{
+    //Position
+    public AnimationCurve curveX, curveY;
+    public Keyframe[] keyframesX;
+    public Keyframe[] keyframesY;
+    //Health & Shield
+    public AnimationCurve curveHealth;
+    public AnimationCurve curveShield;
+
+    public int curveTime;
+    public Transform transform;
+    
+    void Start()
+    {
+        curveX = new AnimationCurve();
+        curveY = new AnimationCurve();
+
+        transform = GetComponent<Transform>();
+        StartCurve();
+        curveTime = 0;
+        StartCoroutine(ChangeKey());
+    }
+    
+    void Update() //For player input
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (isSelected && isAbilityReady)
+            { 
+                Timewarp();
+            }
+        }
+    }
+
+    public void Timewarp() //Main method - Time warps player
+    {
+        int ReturnNumber = GetReturnTime(2);
+        transform.position = new Vector3(keyframesX[ReturnNumber].value, keyframesY[ReturnNumber].value, 0);
+
+    }
+    void StartCurve() //For creating curve
+    {
+        
+        for (int i = 0; i < 10; i++)
+        {
+            curveX.AddKey(i, transform.position.x);
+            curveY.AddKey(i, transform.position.y);
+            //Debug.Log("Key count:" + curveX.keys.Length);
+
+        }
+        keyframesX = curveX.keys;
+        keyframesY = curveY.keys;
+
+    }
+    IEnumerator ChangeKey()
+    {
+        while (true)
+        {
+            if (curveTime != 9)
+            {
+                curveTime++;
+            }
+            else
+            {
+                curveTime = 0;
+            }
+            
+            keyframesX[curveTime].value = (int)transform.position.x;
+            keyframesY[curveTime].value = (int)transform.position.y;
+            curveX.keys = keyframesX;
+            curveY.keys = keyframesY;
+
+            yield return new WaitForSeconds(1.0f);
+
+
+        }
+    }
+    int GetReturnTime(int HowFarGoBack)
+    {
+        Debug.Log("How far go back:" + HowFarGoBack.ToString() + ",curveTime" + curveTime.ToString());
+        int number = 1;
+        if (HowFarGoBack > curveTime)
+        {
+            number = (curveTime - HowFarGoBack) + 10;
+        }
+        else
+        {
+            number = curveTime - HowFarGoBack;
+        }
+        Debug.Log("Return number:" + number.ToString());
+        return number;
+    }
+}
